504f60fbc261918e268ebcfe8b60a0828ef82276
«DeveloperArty» <apple_dev.arty@mail.ru>
2017-06-30 01:15:30 +0300

    Initial Commit

---
+++ Some Cool Pods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Some Cool Pods
+//
+//  Created by Artem Pavlov on 30.06.17.
+//  Copyright © 2017 Den prod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
