231b91662a9054ac08c728e927bc04d9a5b9543c
«DeveloperArty» <apple_dev.arty@mail.ru>
2017-06-30 01:31:04 +0300

    well... chameleon rocks!

--- Some Cool Pods/ViewController.swift
+++ Some Cool Pods/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = GradientColor(.topToBottom,
+                                             frame: view.frame,
+                                             colors: [RandomFlatColor(), RandomFlatColor()])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
